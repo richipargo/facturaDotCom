63e59de877a39eee610519535f8178e7159eac2d
Ricardo Tapia Mancera <rtapia92@gmail.com>
2016-04-08 21:41:04 -0500

    updated with newest api

--- facturaDotCom.gemspec
+++ facturaDotCom.gemspec
@@ -9,8 +9,8 @@ Gem::Specification.new do |spec|
   spec.authors       = ["Ricardo Tapia Mancera"]
   spec.email         = ["rtapia92@gmail.com"]
 
-  spec.summary       = %q{TODO: Write a short summary, because Rubygems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
+  spec.summary       = %q{"A gem to interfaces with Factura.com API endpoints"}
+  spec.description   = %q{"Connection to Factura.com API endpoints for making Mexican Government CFDIs"}
   spec.homepage      = "TODO: Put your gem's website or public repo URL here."
   spec.license       = "MIT"
 
--- lib/facturaDotCom.rb
+++ lib/facturaDotCom.rb
@@ -1,5 +1,106 @@
 require "facturaDotCom/version"
 
 module FacturaDotCom
-  # Your code goes here...
+ class Factura
+    API_URL = "https://factura.com/api/v1"
+
+    def initialize(api_key, api_secret)
+      @api_key = api_key
+      @api_secret = api_secret
+    end
+
+    def find_client(data)
+      find_url = "#{API_URL}/clients/#{data}"
+      res = create_get_request(find_url)
+      Rails.logger.debug("\e[32m#{res}\e[0m")
+      res = JSON.parse(res.body)
+      if res['status'] == "error"
+        result = {
+          status: res['status'],
+          res: res
+        }
+      else
+        result = {
+          status: res['status'],
+          guid: res['Data']['UID']
+        }
+      end
+    end
+
+    def generate_invoice(data)
+      create_url = "#{API_URL}/invoice/create"
+      res = create_post_request(create_url, data.to_json)
+      Rails.logger.debug "\e[32m#{res}\e[0m"
+      res = JSON.parse(res.body)
+      if res['status'] == "error"
+        result = {
+          status: res['status'],
+          res: res
+        }
+      else
+        result = {
+          status: res['status'],
+          guid: res['invoice_uid']
+        }
+      end
+    end
+
+    def create_client(data)
+      create_url = "#{API_URL}/clients/create"
+      res = create_post_request(create_url, data)
+      Rails.logger.debug("\e[32m#{res}\e[0m")
+      res = JSON.parse(res.body)
+      if res['status'] == "error"
+        result = {
+          status: res['status'],
+          res: res
+        }
+      else
+        result = {
+          status: res['status'],
+          guid: res['Data']['UID']
+        }
+      end
+    end
+
+    def update_client(client_guid, data)
+      update_url = "#{API_URL}/clients/#{client_guid}/update"
+      res = create_post_request(update_url, data)
+      Rails.logger.debug("\e[32m#{res}\e[0m")
+      res = JSON.parse(res.body)
+      if res['status'] == "error"
+        result = {
+          status: res['status'],
+          res: res
+        }
+      else
+        result = {
+          status: res['status'],
+          guid: res['Data']['UID']
+        }
+      end
+    end
+
+    protected
+      def create_get_request(url)
+        uri = URI.parse(url)
+        request = Net::HTTP::Get.new(uri)
+        request['Content-Type'] = 'application/json'
+        request['F-API-KEY'] = @api_key
+        request['F-SECRET-KEY'] = @api_secret
+        http = Net::HTTP.start(uri.host, uri.port, use_ssl: true)
+        response = http.request(request)
+      end
+
+      def create_post_request(url, data)
+        uri = URI.parse(url)
+        request = Net::HTTP::Post.new(uri)
+        request['Content-Type'] = 'application/json'
+        request['F-API-KEY'] = @api_key
+        request['F-SECRET-KEY'] = @api_secret
+        request.body = data
+        http = Net::HTTP.start(uri.host, uri.port, use_ssl: true)
+        response = http.request(request)
+      end
+  end
 end
